d603e2d44f846ab22eade0a42b1c8ebd6f4eb24e
garamv20 <garamv20@gmail.com>
2024-02-28 16:08:37 +0900

    project setup

---
+++ src/test/java/com/zero/sports_meetup/SportsMeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.sports_meetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsMeetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
